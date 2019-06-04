7ccef9b799b442ede8f1cd14311d6fe870fb9ee4
chasitu <xie_sha2008@163.com>
2019-06-04 13:39:02 +0800

    update

--- SHCustomQRCode/SHCustomQRCode/ViewController.m
+++ SHCustomQRCode/SHCustomQRCode/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "SHScanViewController.h"
 
 @interface ViewController ()
 
@@ -17,7 +18,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    UIButton *button = [[UIButton alloc] initWithFrame: CGRectMake(0, 0, 150, 54)];
+    [button setBackgroundColor:UIColor.blueColor];
+    [button setTitle:@"点击扫描" forState:0];
+    [button setTitleColor:UIColor.whiteColor forState:0];
+    button.center = self.view.center;
+    [button addTarget:self action:@selector(buttonClick) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
 }
 
+- (void)buttonClick
+{
+    [self.navigationController pushViewController:[SHScanViewController new] animated:YES];
+}
 
 @end
