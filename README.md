5336d4ab46be99fc6e014d84f0a0ecac4abe1486
chasitu <xie_sha2008@163.com>
2019-06-04 14:10:04 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # SHCustomQRCode
 扫描控件封装
+
+使用方法:
+SHCustomQRCodeView *qrcodeView = [[SHCustomQRCodeView alloc] initWithFrame:self.view.bounds];
+qrcodeView.backQRCodeURL = ^(NSString *stringValue) {
+    //do some thing
+};
+qrcodeView.backfailure = ^{
+    //failure
+};
+[self.view addSubview:qrcodeView];
--- SHCustomQRCode/SHCustomQRCode/SHScanViewController.m
+++ SHCustomQRCode/SHCustomQRCode/SHScanViewController.m
@@ -21,21 +21,25 @@
     //页面标题
     self.navigationItem.title = @"扫一扫";
     
-    SHCustomQRCodeView *qrcodeView = [[SHCustomQRCodeView alloc] initWithFrame:self.view.bounds];
     __weak typeof(self) weakSelf = self;
+    SHCustomQRCodeView *qrcodeView = [[SHCustomQRCodeView alloc] initWithFrame:self.view.bounds];
     qrcodeView.backQRCodeURL = ^(NSString *stringValue) {
-        NSLog(@"----------------扫描结果--------------------------\n%@",stringValue);
-        UIAlertController *alertV = [UIAlertController alertControllerWithTitle:@"二维码" message:stringValue preferredStyle:UIAlertControllerStyleAlert];
-        [alertV addAction:[UIAlertAction actionWithTitle:@"知道了" style:UIAlertActionStyleDefault handler:nil]];
-        [alertV addAction:[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil]];
-        [weakSelf presentViewController:alertV animated:YES completion:nil];
+        [weakSelf alertViewTitle:@"扫描成功" Message:stringValue];
+    };
+    qrcodeView.backfailure = ^{
+        [weakSelf alertViewTitle:@"扫描失败" Message:@"扫描失败"];
     };
     [self.view addSubview:qrcodeView];
 }
 
-- (void)dealloc
+- (void)alertViewTitle:(NSString *)title Message:(NSString *)message
 {
-    NSLog(@"----------dealloc---------");
+    __weak typeof(self) weakSelf = self;
+    UIAlertController *alertV = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+    [alertV addAction:[UIAlertAction actionWithTitle:@"知道了" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        [weakSelf.navigationController popViewControllerAnimated:YES];
+    }]];
+    [self presentViewController:alertV animated:YES completion:nil];
 }
 
 @end
